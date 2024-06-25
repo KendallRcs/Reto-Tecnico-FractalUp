968daa38ac240106cb710880197b42eeddc6fa8e
KendallRcs <kendallramiro@gmail.com>
2024-06-24 19:04:09 -0500

    project structure done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+import VueApollo from 'vue-apollo';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
+
+const httpLink = new HttpLink({
+  uri: 'https://countries.trevorblades.com/',
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+Vue.use(VueApollo);
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
